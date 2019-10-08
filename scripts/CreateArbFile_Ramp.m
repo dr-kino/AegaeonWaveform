687f2291df8de5e6c34b428dd592972c13a9b6ee
dr-kino <doctorkinoo@gmail.com>
2019-10-08 23:42:35 +0100

    Included ramp waveform example to the project.

--- scripts/CreateArbFile_Ramp.m
+++ scripts/CreateArbFile_Ramp.m
@@ -1,7 +1,5 @@
 % Ramp waveform example file for the Rigol DG1022 function generator.
 % Saves an .rdf file that can be loaded into the Rigol DG1022
-%
-% Rev. 12/5/2015
 
 clear all
 
@@ -10,7 +8,7 @@ clear all
 Npts = 8192;            % Number of points to save
 s(1) = 0;               % First sample of the waveform
 for n = 2 : Npts        % Loop through all points and create
-    s(n) = s(n-1) + 4;  % Add 4 to each sample to ramp 0 to 16380
+    s(n) = s(n-1) + 4;  % Add 4 to each sample to ramp 0 to 32768
     if s(n) > 32767     % For other increments, wrap to zero
         s(n) = 0;
     end
@@ -23,3 +21,13 @@ Filename = 'Ramp.rdf';                     % Store in this file
 fid = fopen( Filename, 'w' );              % Open the file for writing    
 fwrite( fid, s_fp, 'uint16', 'ieee-le' );  % Write as 16-bit values, little endian
 fclose( fid );                             % Close file
+
+hold on;
+figure(1);
+% Define colors
+green = [0.9 1 0.8];
+red = [1 0.9 0.8];
+
+ylabel('Amplitude');
+xlabel('NSample');
+plot([0:length(s)-1], s, 'r*');
