6c04494af04fb6f277b1208553cda63ea0a6e0de
dr-kino <doctorkinoo@gmail.com>
2020-01-03 11:02:12 +0000

    Calling the header file generator.hpp  in generator.cpp.

---
+++ src/generator.cpp
@@ -0,0 +1,5 @@
+#include "generator.hpp"
+
+void Generator::getSupportedGenerators(std::string *list, size_t listSize)
+{
+}
--- src/generator.hpp
+++ src/generator.hpp
@@ -1,12 +1,15 @@
 #ifndef GENERATOR_HPP
 #define GENERATOR_HPP
 
+#include <string>
+
 class Generator {
     private:
     std::string currentModel_;
     std::string *generatorList_;
     size_t listSize;
+
     public:
     void getSupportedGenerators(std::string *list, size_t listSize);
-}
+};
 #endif // GENERATOR_HPP
