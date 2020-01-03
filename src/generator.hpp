ec47eb2903f6e3ec41831a9d9ab962a4f86bfd20
dr-kino <doctorkinoo@gmail.com>
2020-01-03 10:54:17 +0000

    Inserted method getSupportedGenerators, it should be search in xml file for all supported generators.

--- src/generator.hpp
+++ src/generator.hpp
@@ -1,6 +1,13 @@
 #ifndef GENERATOR_HPP
 #define GENERATOR_HPP
 
+typedef struct Generator {
+} Generator_t;
+
 class Generator {
+    private:
+
+    public:
+    void getSupportedGenerators(std::string *list, size_t listSize);
 }
 #endif // GENERATOR_HPP
